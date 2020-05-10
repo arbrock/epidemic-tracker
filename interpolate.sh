541ff41f55be6ede9aa16311c59bc12f4d9f8656
Andrew Brock <arbrock@andrew.cmu.edu>
2020-05-09 23:26:11 -0500

    Improve R-value calculation to consider only recent cases infectious

--- interpolate.sh
+++ interpolate.sh
@@ -3,8 +3,12 @@ awk -F, '
 BEGIN {
   oldcases=-1;
   WINSIZE=5
+  BASISINTERVAL=7
   for(i = 0; i<WINSIZE; i++) {
     means[i] = -1;
+  }
+  for(i = 0; i<BASISINTERVAL; i++) {
+    basis[i] = -1;
   }
 }
 
@@ -13,20 +17,32 @@ BEGIN {
   cases=$2;
   if(oldcases >= 0) {
     arith=cases-oldcases;
-    geom=cases/oldcases;
+    basissum=0;
+    for(i=BASISINTERVAL-1; i>0; i--) {
+      basis[i] = basis[i-1];
+      if(basis[i] > 0) {
+        basissum += basis[i];
+      }
+    }
+    basis[0] = arith;
+    basissum += basis[0];
+    geom=arith/basissum*BASISINTERVAL;
+    smoothsum = 0;
     for(i=WINSIZE-1; i>0; i--) {
       means[i] = means[i-1];
+      if(means[i] > 0) {
+        smoothsum += log(means[i]);
+      }
     }
     means[0] = geom;
+    smoothsum += log(means[0]);
     if(means[WINSIZE-1] >= 0) {
-      sum = 0;
-      for(i=0; i<WINSIZE; i++) {
-        sum += log(means[i]);
-      }
-      smoothed = exp(sum/WINSIZE);
+      smoothed = exp(smoothsum/WINSIZE);
       print date","cases","arith","geom","smoothed;
-    } else {
+    } else if(basis[BASISINTERVAL-1] >= 0) {
       print date","cases","arith","geom;
+    } else {
+      print date","cases","arith;
     }
   }
   oldcases=cases;
