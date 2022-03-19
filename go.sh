97e6fe862f6f61e40f1a6e73d634db0ef61beddc
Andrew Brock <arbrock@andrew.cmu.edu>
2022-03-18 21:53:01 -0500

    Code for automatically pulling from the data set
    Recently found a link to this from Reddit. It was not easy to find: I'd previously looked for
    it with no success, but now that it's here, expect a cron job.
    
    Data here: https://atc-covid19data-austin.hub.arcgis.com/
    
    This does introduce a dependency on jq for parsing the results JSON returned from ARCGIS.

--- go.sh
+++ go.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd ~/code/cases/
 if [ ! -z "$1" ] ; then
-  echo `date +%Y%m%d`,$1 >> cases.csv
+  if [ "$1" = "auto" ] ; then
+    ./pull.sh
+  else
+    echo `date +%Y%m%d`,$1 >> cases.csv
+  fi
 fi
 guile ./interpolate.scm > interpolated.csv
 gnuplot cases.plot
---
+++ pull.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+json=`mktemp`
+date_csv=$(date +%Y%m%d)
+date_curl=$(date +%m/%d/%Y)
+echo $date_csv $date_curl
+curl "https://services.arcgis.com/0L95CJ0VTaxqcmED/arcgis/rest/services/Daily_Count_COVID_view/FeatureServer/0/query?where=%20(Updated%20%3E%20%27${date_curl}%27%20AND%20RecordStat%20=%20%27Active%27)%20&outFields=*&outSR=4326&f=json" > $json
+
+cat $json
+
+pcr=$(jq .features[0].attributes.PCR $json)
+antigen=$(jq .features[0].attributes.Antigen $json)
+total=$(jq .features[0].attributes.TotalCases $json)
+rm $json
+
+echo "$date_csv,$pcr,$antigen=$total" >> cases.csv
